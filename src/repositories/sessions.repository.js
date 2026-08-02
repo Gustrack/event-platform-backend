ebe6474364629755afd760326f472f45cb66a7c6
gus <gustavoatala1974@gmail.com>
2026-08-02 13:26:50 -0300

    Pre-entrega 1: Refactor arquitectónico inicial para plataforma de eventos

---
+++ src/repositories/sessions.repository.js
@@ -0,0 +1,17 @@
+﻿import sessionsDao from '../dao/sessions.dao.js';
+
+class SessionsRepository {
+  async create(data) {
+    return await sessionsDao.create(data);
+  }
+  
+  async findByEmail(email) {
+    return await sessionsDao.findByEmail(email);
+  }
+  
+  async findById(id) {
+    return await sessionsDao.findById(id);
+  }
+}
+
+export default new SessionsRepository();
---
+++ src/utils/index.js
@@ -0,0 +1,30 @@
+﻿import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from '../config/config.js';
+
+export const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+export const comparePasswords = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+export const generateToken = (payload) => {
+  return jwt.sign(payload, config.jwtSecret, { expiresIn: '7d' });
+};
+
+export const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, config.jwtSecret);
+  } catch (error) {
+    return null;
+  }
+};
+
+export default {
+  hashPassword,
+  comparePasswords,
+  generateToken,
+  verifyToken
+};
